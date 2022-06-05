ac3aba784b88fc868c0fd177c5242eacf57a21ab
Evan Garcia <evanag116@yahoo.com>
2022-06-05 17:53:33 -0500

    completed in js

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,27 @@
-const calculateMode = () => {
+exports.calculateMode = function(arr) {
+    
+    let modes = []
+    let currentMax = 0
 
+    for (let i = 0; i < arr.length; i++) {
+
+        let currentMaxCount = arr.filter(x => x==arr[i]).length
+        
+        if (currentMaxCount > currentMax) {
+            currentMax = currentMaxCount
+        }
+    }
+
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr.filter(x => x==arr[i]).length == currentMax ^ modes.includes(arr[i])) {
+            modes.push(arr[i])
+        }
+        
+    }
+    return modes
 }
+
+
+
+
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,10 @@
-// Write your own unit tests here!
+var mode = require("./calculateMode");
+
+
+
+console.log(mode.calculateMode([1,2,3,4,3]) == [3,4])
+console.log(mode.calculateMode([1,2,3]) == [1,2,3])
+console.log(mode.calculateMode(["Evan", "Anabelle", "Birdie", "Evan"]) == ["Evan"])
+console.log(mode.calculateMode([1.5, 2, 3, 4, 1.5, 2, 4]) == [1.5, 2, 4])
+console.log(mode.calculateMode([true, true, false]) == [true])
+
